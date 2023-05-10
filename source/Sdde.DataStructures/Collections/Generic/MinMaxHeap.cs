6d1d696a8b180fdf5fa3ae9f71efa62a8ee1d62e
Matthew Andrew <mandrew333@msn.com>
2023-05-09 18:24:37 -0700

    feat: added Max Heapify

--- source/Sdde.DataStructures/Collections/Generic/MinMaxHeap.cs
+++ source/Sdde.DataStructures/Collections/Generic/MinMaxHeap.cs
@@ -1,103 +1,236 @@
-﻿namespace Sdde.Collections.Generic;
+﻿using System.Collections;
 
-public class MinMaxHeap<T> where T : IComparable<T>
+namespace Sdde.Collections.Generic;
+
+public class MinMaxHeap<T> : IEnumerable<T> where T : IComparable<T>
 {
-    private const int DefaultCapacity = 100;
+    private int _count;
+    public const int DefaultCapacity = 100;
     protected readonly IEnumerable<T> heap;
     protected readonly HeapType heapType;
     private readonly IComparer<T> comparer;
     public bool IsMinHeap => heapType == HeapType.Min;
     public bool IsEmpty => Count == 0;
-    public int Count { get { return heap.Count()>= 0 ? heap.Count() : 0;} private set { Count += value; } }
+    public int Count { get { return _count >= 0 ? _count : 0;} private set { _count += value; } }
     public int Capacity => heap.Count();
     public IComparer<T> Comparer => comparer ?? Comparer<T>.Default;
 
-    public MinMaxHeap() : this(new T[DefaultCapacity], HeapType.Min, Comparer<T>.Default) { }
-
-    public MinMaxHeap(IEnumerable<T> array) : this(array, HeapType.Min, Comparer<T>.Default)
+    public MinMaxHeap() : this(new T[DefaultCapacity], HeapType.Min, Comparer<T>.Default) => this._count = 0;
+    public MinMaxHeap(IEnumerable<T> array) : this(array, HeapType.Min, Comparer<T>.Default) { }
+    public MinMaxHeap(IEnumerable<T> array, HeapType heapType, IComparer<T> comparer)
     {
-        this.heap = array;
-    }
-
-    public MinMaxHeap(HeapType heapType) : this(new T[DefaultCapacity], heapType, Comparer<T>.Default)
-    {
-        this.heapType = heapType;
-    }
-
-    public MinMaxHeap(IComparer<T> comparer) : this(new T[DefaultCapacity], HeapType.Min, comparer)
-    {
-        this.comparer = comparer;
-    }
-
-    public MinMaxHeap(
-        HeapType heapType,
-        IComparer<T> comparer) : this(new T[DefaultCapacity], heapType, comparer)
-    {
-        this.comparer = comparer;
-        this.heapType = heapType;
-    }
-
-    public MinMaxHeap(
-        IEnumerable<T> array,
-        HeapType heapType,
-        IComparer<T> comparer)
-    {
-        this.heap = array;
+        this.Count = array.Count();
         this.heapType = heapType;
         this.comparer = comparer;
-        // this.heap = buildHeap(array);
+        this.heap = HeapSort(array.ToArray(), this.Count);
+    }
+    public MinMaxHeap(HeapType heapType) : this(new T[DefaultCapacity], heapType, Comparer<T>.Default) => this._count = 0;
+    public MinMaxHeap(IComparer<T> comparer) : this(new T[DefaultCapacity], HeapType.Min, comparer) => this._count = 0;
+    public MinMaxHeap(HeapType heapType, IComparer<T> comparer) : this(new T[DefaultCapacity], heapType, comparer) => this._count = 0;
+    public MinMaxHeap(IEnumerable<T> array, HeapType heapType) : this(array, heapType, Comparer<T>.Default) { }
+    public MinMaxHeap(IEnumerable<T> array, IComparer<T> comparer) : this(array, HeapType.Min, comparer) { }
+
+
+    public IEnumerable<T> HeapSort(T[] array, int size)
+    {
+        ArgumentNullException.ThrowIfNull(array);
+        if (size <= 1) return array;
+        // if (array.Count() <= 1) return array;
+        // int startIndex = (size / 2);
+        int startIndex = (size / 2) - 1;
+        // int startIndex = size;
+        for (int currentIndex = startIndex; currentIndex >= 0; currentIndex--)
+        {
+            MaxHeapify(array, size, currentIndex);
+            // MinHeapify(array, currentIndex, size);
+        }
+
+        // for (int currentIndex = size - 1; currentIndex >= 0; currentIndex--)
+        // {
+        //     Swap(ref array.ToArray()[0], ref array.ToArray()[currentIndex]);
+        //     // Swap(ref array[0], ref array[currentIndex]);
+        //     Heapify(array, currentIndex, 0);
+        // }
+
+        return array;
     }
 
+    private void MaxHeapify(T[] array, int size, int index)
+    {
+        var current = index;
 
+        if (GetLeftChildIndex(index) < size
+            && array.ElementAt(GetLeftChildIndex(index)).CompareTo(array.ElementAt(current)) > 0)
+        {
+            current = GetLeftChildIndex(index);
+        }
 
+        if (GetRightChildIndex(index) < size
+            && array.ElementAt(GetRightChildIndex(index)).CompareTo(array.ElementAt(current)) > 0)
+        {
+            current = GetRightChildIndex(index);
+        }
 
+        if (current != index)
+        {
+            Swap(ref array[index], ref array[current]);
+            MaxHeapify(array, size, current);
+        }
+    }
 
+    private void MinHeapify(T[] array, int size, int index)
+    {
+        var current = index;
 
+        if (GetLeftChildIndex(index) < size
+            && array.ElementAt(GetLeftChildIndex(index)).CompareTo(array.ElementAt(current)) < 0)
+        {
+            current = GetLeftChildIndex(index);
+        }
 
+        if (GetRightChildIndex(index) < size
+            && array.ElementAt(GetRightChildIndex(index)).CompareTo(array.ElementAt(current)) < 0)
+        {
+            current = GetRightChildIndex(index);
+        }
 
+        if (current != index)
+        {
+            Swap(ref array[index], ref array[current]);
+            MinHeapify(array, current, size);
+        }
+    }
 
+    /// <summary>
+    public void siftDown(int index)
+    {
+        ArgumentNullException.ThrowIfNull(heap);
+        if (this.IsEmpty || this.Count == 1 || index <= 0 || index > this.Count) return;
 
-    // public IEnumerable<T> buildHeap(IEnumerable<T> array)
-    // {
-    //     // Write your code here.
-    //     return new List<T>();
-    // }
+        var (left, right) = GetChildren(index);
+        if (left.CompareTo(right) < 0)
+        {
+            Swap(ref left, ref heap.ToArray()[index]);
+            siftDown(GetLeftChildIndex(index));
+        }
+        else if (left.CompareTo(right) < 0)
+        {
+            Swap(ref right, ref heap.ToArray()[index]);
+            siftDown(GetRightChildIndex(index));
+        }
+        else
+        {
+            return;
+        }
+    }
 
-    // public void siftDown(int currentIdx, int endIdx, List<int> heap)
-    // {
-    //     // Write your code here.
-    // }
+    /// <summary>
+    /// Sifts the element at the given index up the heap
+    /// </summary>
+    public void siftUp(int index)
+    {
+        ArgumentNullException.ThrowIfNull(heap);
+        if (this.IsEmpty || this.Count == 1 || index <= 0 || index > this.Count) return;
 
-    // public void siftUp(int currentIdx, List<int> heap)
-    // {
-    //     // Write your code here.
-    // }
+        if (Compare(heap.ElementAt(index), heap.ElementAt(GetParentIndex(index))) < 0)
+        {
+            Swap(ref heap.ToArray()[index], ref heap.ToArray()[GetParentIndex(index)]);
+            siftUp(GetParentIndex(index));
+        }
+    }
 
-    // public int Peek()
-    // {
-    //     // Write your code here.
-    //     return -1;
-    // }
+    /// <summary>
+    /// Swaps the elements at the given indices
+    /// </summary>
+    public void Swap(ref T left, ref T right)
+    {
+        var temp = left;
+        left = right;
+        right = temp;
+    }
 
-    // public int Remove()
-    // {
-    //     // Write your code here.
-    //     return -1;
-    // }
+    /// <summary>
+    /// Compares two elements of the heap
+    /// </summary>
+    public int Compare(T left, T right)
+    {
+        // encapsulates the Min vs Max logic
+        return IsMinHeap ? Comparer.Compare(left, right) : Comparer.Compare(right, left);
+    }
 
-    // public void Insert(int value)
-    // {
-    //     // Write your code here.
-    // }
+    /// <summary>
+    /// Returns the children of the element at the given index
+    /// </summary>
+    public (T, T) GetChildren(int index)
+    {
+        ArgumentNullException.ThrowIfNull(heap);
+        T left = heap.ElementAt(GetLeftChildIndex(index));
+        T right = heap.ElementAt(GetRightChildIndex(index));
+        return (left, right);
+    }
 
-    // public void Swap<T>(ref T left, ref T right)
-    // {
-    //     var temp = left;
-    //     left = right;
-    //     right = temp;
-    // }
+    /// <summary>
+    /// Returns the heap property (Min/Max) value
+    /// </summary>
+    public T Peek()
+    {
+        ArgumentNullException.ThrowIfNull(heap);
+        return heap.ElementAt(0);
+    }
+
+    /// <summary>
+    /// Removes the root element from the heap
+    /// </summary>
+    public T Remove()
+    {
+        ArgumentNullException.ThrowIfNull(heap);
+        T value = heap.ElementAt(0);
+        heap.ToArray()[0] = heap.ElementAt(this.Count - 1);
+        this.Count--;
+        return value;
+    }
+
+    public void Insert(T value)
+    {
+        ArgumentNullException.ThrowIfNull(heap);
+        heap.ToArray()[this.Count] = value;
+        this.Count++;
+    }
+
+    public void Clear()
+    {
+        heap.ToList().Clear();
+        this.Count = 0;
+    }
+
+    public bool Contains(T value)
+    {
+        ArgumentNullException.ThrowIfNull(heap);
+        return heap.Contains(value);
+    }
+
+    private int GetLeftChildIndex(int elementIndex) => 2 * elementIndex + 1;
+    private int GetRightChildIndex(int elementIndex) => 2 * elementIndex + 2;
+    private int GetParentIndex(int elementIndex) => (elementIndex - 1) / 2;
+
+    private bool HasLeftChild(int elementIndex) => GetLeftChildIndex(elementIndex) < this.Capacity;
+    private bool HasRightChild(int elementIndex) => GetRightChildIndex(elementIndex) < this.Capacity;
+    private bool IsRoot(int elementIndex) => elementIndex == 0;
+
+    private T GetLeftChild(int elementIndex) => heap.ElementAt(GetLeftChildIndex(elementIndex));
+    private T GetRightChild(int elementIndex) => heap.ElementAt(GetRightChildIndex(elementIndex));
+    private T GetParent(int elementIndex) => heap.ElementAt(GetParentIndex(elementIndex));
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
 }
-
 
 public enum HeapType
 {
--- tests/Sdde.DataStructures.Tests.Unit/Collections/MinMaxHeapTests.cs
+++ tests/Sdde.DataStructures.Tests.Unit/Collections/MinMaxHeapTests.cs
@@ -14,74 +14,95 @@ public class MinMaxHeapTests
     }
 
     [Fact]
-    public void InitializeConstructor_Empty_Returns()
+    public void InitializeConstructor_Default_EmptyMinDefault()
     {
         // Arrange
-        var heap = new MinMaxHeap<int>();
+        var heap = new MinMaxHeap<int>();;
 
         // Act
 
         // Assert
         using var scope = new AssertionScope();
+        heap.IsEmpty.Should().BeTrue();
+        heap.Count.Should().Be(0);
+        heap.Capacity.Should().Be(MinMaxHeap<int>.DefaultCapacity);
+        heap.IsMinHeap.Should().BeTrue();
         heap.Comparer.Should().Be(Comparer<int>.Default);
-        heap.IsEmpty.Should().BeTrue();
-        heap.IsMinHeap.Should().BeTrue();
-        heap.Count.Should().Be(0);
-        heap.Capacity.Should().Be(0);
     }
 
     [Theory]
     [MemberData(nameof(MinMaxHeapTestsData.TestData), MemberType = typeof(MinMaxHeapTestsData))]
-    public void InitializeConstructor_Data_HeapType_Comparer_Returns()
+    public void InitializeConstructor_AllParameters_NonEmptyMinDefault<T>(IEnumerable<T> data) where T : IComparable<T>
     {
         // Arrange
-        var heap = new MinMaxHeap<int>();
-
-        // Act
-
-        // Assert
-        using var scope = new AssertionScope();
-        heap.IsEmpty.Should().BeTrue();
-        heap.IsMinHeap.Should().BeTrue();
-        heap.Count.Should().Be(0);
-    }
-
-    [Theory]
-    [MemberData(nameof(MinMaxHeapTestsData.TestData), MemberType = typeof(MinMaxHeapTestsData))]
-    public void InitializeConstructor_Data_Returns<T>(IEnumerable<T> data) where T : IComparable<T>
-    {
-        // Arrange
-        var values = fixture.CreateMany<T>().ToArray();
         var heap = new MinMaxHeap<T>(data, HeapType.Min, Comparer<T>.Default);
 
         // Act
 
         // Assert
         using var scope = new AssertionScope();
-        heap.Comparer.Should().Be(Comparer<int>.Default);
         heap.IsEmpty.Should().BeFalse();
+        heap.Count.Should().Be(data.Count());
+        heap.Capacity.Should().Be(data.Count());
         heap.IsMinHeap.Should().BeTrue();
-        heap.Count.Should().Be(10);
-        heap.Capacity.Should().Be(10);
+        heap.Comparer.Should().Be(Comparer<int>.Default);
+    }
+
+    [Theory]
+    [MemberData(nameof(MinMaxHeapTestsData.TestData), MemberType = typeof(MinMaxHeapTestsData))]
+    public void InitializeConstructor_TheoryDataWithDefaults_NonEmptyMinDefault<T>(IEnumerable<T> data) where T : IComparable<T>
+    {
+        // Arrange
+        var heap = new MinMaxHeap<T>(data);
+
+        // Act
+
+        // Assert
+        using var scope = new AssertionScope();
+        heap.IsEmpty.Should().BeFalse();
+        heap.Count.Should().Be(data.Count());
+        heap.Capacity.Should().Be(data.Count());
+        heap.IsMinHeap.Should().BeTrue();
+        heap.Comparer.Should().Be(Comparer<int>.Default);
     }
 
     [Fact]
-    public void InitializeConstructor_HeapType_Returns()
+    public void InitializeConstructor_FixtureDataWithDefaults_NonEmptyMinDefault()
     {
         // Arrange
-        var heap = new MinMaxHeap<int>(HeapType.Min);
+        var values = fixture.CreateMany<int>().ToArray();
+        var heap = new MinMaxHeap<int>(values);
+
+        // Act
+
+        // Assert
+        using var scope = new AssertionScope();
+        heap.IsEmpty.Should().BeFalse();
+        heap.Count.Should().Be(values.Length);
+        heap.Capacity.Should().Be(values.Length);
+        heap.IsMinHeap.Should().BeTrue();
+        heap.Comparer.Should().Be(Comparer<int>.Default);
+    }
+
+    [Fact]
+    public void InitializeConstructor_MaxHeapType_EmptyMax()
+    {
+        // Arrange
+        var heap = new MinMaxHeap<int>(HeapType.Max);
 
         // Act
 
         // Assert
         using var scope = new AssertionScope();
         heap.IsEmpty.Should().BeTrue();
-        heap.IsMinHeap.Should().BeTrue();
         heap.Count.Should().Be(0);
+        heap.Capacity.Should().Be(MinMaxHeap<int>.DefaultCapacity);
+        heap.IsMinHeap.Should().BeFalse();
+        heap.Comparer.Should().Be(Comparer<int>.Default);
     }
 
     [Fact]
-    public void InitializeConstructor_Comparer_Returns()
+    public void InitializeConstructor_DefaultComparer_ReturnsDefault()
     {
         // Arrange
         var heap = new MinMaxHeap<int>(Comparer<int>.Default);
@@ -91,59 +112,120 @@ public class MinMaxHeapTests
         // Assert
         using var scope = new AssertionScope();
         heap.IsEmpty.Should().BeTrue();
-        heap.IsMinHeap.Should().BeTrue();
         heap.Count.Should().Be(0);
+        heap.Capacity.Should().Be(MinMaxHeap<int>.DefaultCapacity);
+        heap.IsMinHeap.Should().BeTrue();
+        heap.Comparer.Should().Be(Comparer<int>.Default);
     }
 
     [Fact]
-    public void InitializeConstructor_Data_HeapType_Returns()
+    public void InitializeConstructor_FixtureDataWithMaxHeapType_Returns()
     {
         // Arrange
-        var heap = new MinMaxHeap<int>();
+        var values = fixture.CreateMany<int>().ToArray();
+        var heap = new MinMaxHeap<int>(values, HeapType.Max);
 
         // Act
 
         // Assert
         using var scope = new AssertionScope();
-        heap.IsEmpty.Should().BeTrue();
-        heap.IsMinHeap.Should().BeTrue();
-        heap.Count.Should().Be(0);
+        heap.IsEmpty.Should().BeFalse();
+        heap.Count.Should().Be(values.Length);
+        heap.Capacity.Should().Be(values.Length);
+        heap.IsMinHeap.Should().BeFalse();
+        heap.Comparer.Should().Be(Comparer<int>.Default);
     }
 
     [Fact]
-    public void InitializeConstructor_Data_Comparer_Returns()
+    public void InitializeConstructor_FixtureDataWithComparer_Returns()
     {
         // Arrange
-        var heap = new MinMaxHeap<int>();
+        var values = fixture.CreateMany<int>().ToArray();
+        var heap = new MinMaxHeap<int>(values, Comparer<int>.Default);
 
         // Act
 
         // Assert
         using var scope = new AssertionScope();
-        heap.IsEmpty.Should().BeTrue();
+        heap.IsEmpty.Should().BeFalse();
+        heap.Count.Should().Be(values.Length);
+        heap.Capacity.Should().Be(values.Length);
         heap.IsMinHeap.Should().BeTrue();
-        heap.Count.Should().Be(0);
+        heap.Comparer.Should().Be(Comparer<int>.Default);
     }
 
     [Fact]
     public void InitializeConstructor_HeapType_Comparer_Returns()
     {
         // Arrange
-        var heap = new MinMaxHeap<int>(HeapType.Min, Comparer<int>.Default);
+        var heap = new MinMaxHeap<int>(HeapType.Max, Comparer<int>.Default);
 
         // Act
 
         // Assert
         using var scope = new AssertionScope();
         heap.IsEmpty.Should().BeTrue();
-        heap.IsMinHeap.Should().BeTrue();
         heap.Count.Should().Be(0);
+        heap.Capacity.Should().Be(MinMaxHeap<int>.DefaultCapacity);
+        heap.IsMinHeap.Should().BeFalse();
+        heap.Comparer.Should().Be(Comparer<int>.Default);
     }
 
+    [Fact]
+    public void BuildMaxHeap()
+    {
+        // Arrange
+        int[] values = new int[] { 1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
+        int[] expectedValues = new int[] { 17, 15, 13, 9, 6, 5, 10, 4, 8, 3, 1 };
+        values = new int[] {12, 2, 24, 51, 8, -5};
+        expectedValues = new int[] { 51, 12, 24, 2, 8, -5 };
 
+        // Act
+        MinMaxHeap<int> heap = new MinMaxHeap<int>(values, HeapType.Max, Comparer<int>.Default);
+
+        // Assert
+        heap.Count.Should().Be(values.Length);
+        heap.Capacity.Should().Be(values.Length);
+        heap.IsMinHeap.Should().BeFalse();
+        heap.Comparer.Should().Be(Comparer<int>.Default);
+        // heap.Should().BeEquivalentTo(expectedValues);
+    }
+
+    [Fact]
+    public void BuildMinHeap()
+    {
+        // Arrange
+        int[] values = new int[] { 73, 57, 49, 99, 133, 20, 1 };
+        int[] expectedValues = new int[] { 1, 20, 49, 57, 73, 99, 133 };
+
+        // Act
+        MinMaxHeap<int> heap = new MinMaxHeap<int>(values, HeapType.Min, Comparer<int>.Default);
+
+        // Assert
+        heap.Count.Should().Be(values.Length);
+        heap.Capacity.Should().Be(values.Length);
+        heap.IsMinHeap.Should().BeTrue();
+        heap.Comparer.Should().Be(Comparer<int>.Default);
+        // heap.Should().BeEquivalentTo(expectedValues);
+    }
+
+    [Fact]
+    public void TestIncreaseHeapSize()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
 
     [Fact]
     public void Insert()
+    {
+    }
+
+    [Fact]
+    public void Remove()
     {
     }
 
@@ -152,12 +234,3 @@ public class MinMaxHeapTests
     }
 }
 
-// internal class MinMaxHeap
-// {
-//     private IEnumerable<object> data;
-
-//     public MinMaxHeap(IEnumerable<object> data)
-//     {
-//         this.data = data;
-//     }
-// }
